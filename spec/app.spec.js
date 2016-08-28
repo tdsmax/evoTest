160b35d9283ca151aba60811de7e19702db0eb8a
Lakha Singh <polizic@yahoo.co.in>
2016-08-28 19:54:28 +0530

    Created Setup for testing and added spec for app.js

---
+++ spec/app.spec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Initial State
+describe('App Module', function () {
+
+
+  it('should create connection on connectionReady Event', function(){
+    expect( true ).toBe( true );
+  });
+
+  it('should update table on userUpdate event', function(){
+    expect( true ).toBe( true );
+  });
+
+  it('should remove table on removeTable event', function(){
+    expect( true ).toBe( true );
+  });
+});
